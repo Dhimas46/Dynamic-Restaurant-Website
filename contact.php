383d6e6f0a1d3ee5118e56bfaa355d379dc4dcf8
Dhimas Wahyu Prayogi <44389333+Dhimas46@users.noreply.github.com>
2023-02-17 10:00:55 +0700

    Update contact.php

--- contact.php
+++ contact.php
@@ -2,49 +2,7 @@
   define("TITLE", "Contact | Franklin's Fine Dinning");
   include('includes/header.php');
 
-  /*
-    NOTE:
-    In the form in contact.php, the name text field has the name "name"
-    If the user submits the form, the $_POST['name'] variable will be
-    automatically created, and will contain the text they typed into
-    the field. The $_POST['email'] variable will contain whatever they typed
-    into the email field.
-
-
-    PHP used in this script:
-
-    preg_match()
-    - Perform a regular expression match
-    - http://ca2.php.net/preg_match
-
-    isset()
-    - Determine if a variable is set and is not NULL
-    - http://ca2.php.net/manual/en/function.isset.php
-
-    $_POST
-    - An associative array of variables passed to the current script via the HTTP POST method.
-    - http://www.php.net/manual/en/reserved.variables.post.php
-
-    trim()
-    - Strip whitespace (or other characters) from the beginning and end of a string
-    - http://www.php.net/manual/en/function.trim.php
-
-    exit
-    - Output a message and terminate the current script
-    - http://www.php.net//manual/en/function.exit.php
-
-    die()
-    - Equivalent to exit
-    - http://ca1.php.net/manual/en/function.die.php
-
-    wordwrap()
-    - Wraps a string to a given number of characters
-    - http://ca1.php.net/manual/en/function.wordwrap.php
-
-    mail()
-    - Send mail
-    - http://ca1.php.net/manual/en/function.mail.php
-  */
+ 
 
 ?>
 
@@ -75,7 +33,7 @@
       }
 
       // Add the recipient email to a variable
-      $to = "gill454@gmail.com";
+      $to = "dhimaswahyu0846@gmail.com";
 
       // Create a subject
       $subject = "$name sent you a message via your contact form";
@@ -105,7 +63,7 @@
 
   ?>
       <!-- Show success message after email has sent -->
-      <h5>Thanks for contacting Franklin's!</h5>
+      <h5>Thanks for contacting Admin!</h5>
       <p>Please allow 24 hours for a response.</p>
       <p><a href="index.php" class="button block">&laquo; Go to Home Page</a></p>
 
